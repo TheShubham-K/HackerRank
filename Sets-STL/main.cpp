38d6952b002b69e57cada281d2b66a8c22de7d26
shubham-sys <subham.kumar032@gmail.com>
2020-06-20 18:53:17 +0530

    Sets STL

---
+++ Sets-STL/main.cpp
@@ -0,0 +1,37 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    set<int> s;
+    for(int i=0;i<n;i++){
+        int type;
+        int value;
+        cin>>type>>value;
+        if(type==1){
+            s.insert(value);
+        }
+        else if (type==2) {
+        s.erase(value);
+        }else if (type == 3) {
+        auto it = s.find(value);
+        if(it != s.end()){
+            cout<<"Yes\n";
+            }else{
+                cout<<"No\n";
+            }
+        }
+    }
+    return 0;
+}
+
+
+
