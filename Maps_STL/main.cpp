7b932ca65292fb99567b693e388a5ac44fe0e65a
shubham-sys <subham.kumar032@gmail.com>
2020-06-20 18:51:46 +0530

    Maps STL

---
+++ Maps_STL/main.cpp
@@ -0,0 +1,44 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int n;
+    cin>>n;
+    map<string,int>m;
+    for(int i=0;i<n;i++){
+        int type,mark;
+        string name;
+        cin>>type>>name;
+        if(type==1){
+            cin>>mark;
+            auto it = m.find(name);
+            if(it != m.end()){
+                m[name] += mark;
+            }else{
+                m[name] = mark;
+            }
+        }else if(type==2){
+            m.erase(name);
+        }else if (type==3) {
+        auto it = m.find(name);
+        if(it != m.end()){
+            cout<<m[name]<<"\n";
+        }else{
+            cout<<"0\n";
+        }
+        }
+
+    }
+    return 0;
+}
+
+
+
